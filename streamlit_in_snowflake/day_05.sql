3ddcc731170a4a9d23ebf425378d02dc2bffad12
David Sanchez <david.sanchez@datarsys.co.uk>
2026-01-07 15:11:53 +0000

    doc: add info re: trial accounts

--- streamlit_in_snowflake/day_05.sql
+++ streamlit_in_snowflake/day_05.sql
@@ -1,9 +1,28 @@
 ALTER GIT REPOSITORY IF EXISTS dsmdavid_github_30days FETCH;
 
+/* the below does not work in trial accounts as external access is not supported 
+CREATE OR REPLACE NETWORK RULE dsmdavid_30days_day_05
+  TYPE = HOST_PORT
+  MODE = EGRESS
+  VALUE_LIST = ('hpclaser.co.uk','bit.ly');
+
+
+CREATE OR REPLACE EXTERNAL ACCESS INTEGRATION dsmdavid_30days_day_05
+  ALLOWED_NETWORK_RULES = (dsmdavid_30days_day_05)
+  ENABLED = TRUE;
+
 CREATE STREAMLIT IF NOT EXISTS dsmdavid_30days_day_05
   FROM @THIRTY_DAYS.COMMON.DSMDAVID_GITHUB_30DAYS/branches/main/app
   MAIN_FILE = 'day05.py'
   QUERY_WAREHOUSE = COMPUTE_WH;
+  EXTERNAL_ACCESS_INTEGRATIONS = (dsmdavid_30days_day_05);
+  
+-- end of comment to use the below instead of the above */
 
+CREATE STREAMLIT IF NOT EXISTS dsmdavid_30days_day_05
+  FROM @THIRTY_DAYS.COMMON.DSMDAVID_GITHUB_30DAYS/branches/main/app
+  MAIN_FILE = 'day05.py'
+  QUERY_WAREHOUSE = COMPUTE_WH
+;
 
 ALTER STREAMLIT dsmdavid_30days_day_05 ADD LIVE VERSION FROM LAST;
